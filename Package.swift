837f987fc65bd9f5970979bee8bf8c1af93621c2
kwangho <kwangho2716@hanpass.com>
2022-10-21 09:08:06 +0900

    [Test] SPM

--- Package.swift
+++ Package.swift
@@ -3,15 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "AlamoPackage",
+    name: "SPM Package",
+    platforms: [.iOS(.v13)],
     products: [
-        .library(name: "AlamoPackage", type: .dynamic, targets: ["AlamoPackage"]),
-        .library(name: "OthersPackage", type: .static, targets: ["OthersPackage"])
+        // dynamic, static, none
+        // use_framework: -> static / unuse_framework: -> dynamic
+        .library(name: "AlamoPackage", type: .static, targets: ["AlamoPackage"]),
+        .library(name: "OthersPackage", type: .dynamic, targets: ["OthersPackage"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "4.9.0"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "8.15.0"),
-        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.0"),
+        .package(url: "https://github.com/google/GoogleDataTransport.git", exact: "9.1.2"),
+        .package(url: "https://github.com/google/GoogleUtilities.git", exact: "7.7.0"),
+        .package(url: "https://github.com/google/promises.git", exact: "2.1.0"),
+
         .package(url: "https://github.com/zendesk/answer_bot_sdk_ios.git", from: "2.2.0"),
         .package(url: "https://github.com/zendesk/chat_sdk_ios.git", from: "2.12.0"),
         .package(url: "https://github.com/zendesk/support_sdk_ios.git", from: "5.5.0"),
@@ -26,18 +32,16 @@ let package = Package(
         .target(
             name: "OthersPackage",
             dependencies: [
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDynamicLinks", package: "firebase-ios-sdk"),
-                .product(name: "PINRemoteImage", package: "PINRemoteImage"),
+                .product(name: "GoogleDataTransport", package: "GoogleDataTransport"),
+
                 .product(name: "ZendeskAnswerBotSDK", package: "answer_bot_sdk_ios"),
                 .product(name: "ZendeskChatSDK", package: "chat_sdk_ios"),
                 .product(name: "ZendeskSupportSDK", package: "support_sdk_ios"),
                 .product(name: "ZendeskSDKMessaging", package: "sdk_messaging_ios"),
             ]),
-        .testTarget(
-            name: "MyPackageTests",
-            dependencies: ["AlamoPackage"]),
     ]
 )
